96bbc54eb352e380b5821225822607b21851f4fb
Anna Prokopenko <prokopenchik22@mail.ru>
2017-04-10 15:10:00 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,8 @@
+//= ../../libs/jquery/dist/jquery.min.js
+//= ../../libs/bootstrap/dist/js/bootstrap.min.js
+
+//= partials/particle.js
+//= partials/progressBar.js
+//= partials/perehod.js
+//= partials/ava_foto.js
+//= partials/resume_cvs.js
